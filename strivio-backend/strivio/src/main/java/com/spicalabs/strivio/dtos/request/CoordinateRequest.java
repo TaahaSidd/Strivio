971b71406d324a79c2d7d546b98b93c47e139f98
Sidd <tahasidd33@gmail.com>
2026-06-15 01:57:16 +0530

    added request dto

--- strivio-backend/strivio/src/main/java/com/spicalabs/strivio/dtos/request/CoordinateRequest.java
+++ strivio-backend/strivio/src/main/java/com/spicalabs/strivio/dtos/request/CoordinateRequest.java
@@ -1,5 +1,19 @@
 package com.spicalabs.strivio.dtos.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CoordinateRequest {
-    
+
+    @NotNull(message = "Longitude is required")
+    private Double longitude;
+    @NotNull(message = "Latitude is required")
+    private Double latitude;
 }
--- strivio-backend/strivio/src/main/java/com/spicalabs/strivio/dtos/request/PathCreationRequest.java
+++ strivio-backend/strivio/src/main/java/com/spicalabs/strivio/dtos/request/PathCreationRequest.java
@@ -1,5 +1,28 @@
 package com.spicalabs.strivio.dtos.request;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PathCreationRequest {
     
+    @NotNull(message = "User ID is required")
+    private UUID userId;
+    @NotNull(message = "Path Name is required")
+    private String pathName;
+
+    private String description;
+    private BigDecimal distance;
+    @NotNull(message = "A path must contain at least two coordinates")
+    private List<CoordinateRequest> coordinates;
 }
