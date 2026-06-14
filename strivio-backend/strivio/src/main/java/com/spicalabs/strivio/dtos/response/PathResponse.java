c1e25a91cc6b96b5205a3d34b7589e6569d034d6
Sidd <tahasidd33@gmail.com>
2026-06-15 01:57:54 +0530

    added response dto

--- strivio-backend/strivio/src/main/java/com/spicalabs/strivio/dtos/response/PathResponse.java
+++ strivio-backend/strivio/src/main/java/com/spicalabs/strivio/dtos/response/PathResponse.java
@@ -1,5 +1,27 @@
 package com.spicalabs.strivio.dtos.response;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PathResponse {
-    
+
+    private UUID id;
+    private String pathName;
+    private String description;
+    private BigDecimal distance;
+    private OffsetDateTime createdAt;
+    private String creatorUsername;
+    List<PointResponse> coordinates;
+
 }
--- strivio-backend/strivio/src/main/java/com/spicalabs/strivio/dtos/response/PointResponse.java
+++ strivio-backend/strivio/src/main/java/com/spicalabs/strivio/dtos/response/PointResponse.java
@@ -1,5 +1,16 @@
 package com.spicalabs.strivio.dtos.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PointResponse {
-    
+
+    private double longitude;
+    private double latitude;
 }
